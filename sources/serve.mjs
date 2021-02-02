d59082de43d89be8c8bea5d18e2923e9a93d0a16
Amin NAIRI <nairi.amin@gmail.com>
2021-02-02 09:19:32 +0100

    :bug: fixed an issue where the port could be a non-integer

--- sources/serve.mjs
+++ sources/serve.mjs
@@ -11,7 +11,7 @@ if (getOptionValue({name: "--help", fallback: false, boolean: true, options})) {
   console.log("--help: display this message.");
   console.log("--folder: The folder from where to serve the static folder (default to the current folder).");
   console.log("--host: The host from which to listen (default to 127.0.0.1).");
-  console.log("--port: The port from which to listen (default to 8000).");
+  console.log("--port: The port from which to listen (default to 8000). Fail if not integer.");
   console.log("--spa: Whether to route all request to the current folder's index.html file (default to false).");
   console.log("--verbose: Display error message on the console instead of hidding them.");
   console.log("Example: serve --folder public --host 0.0.0.0 --port 5000 --spa");
@@ -21,12 +21,12 @@ if (getOptionValue({name: "--help", fallback: false, boolean: true, options})) {
 
 const folder = getOptionValue({name: "--folder", fallback: "", boolean: false, options});
 const host = getOptionValue({name: "--host", fallback: "127.0.0.1", boolean: false, options});
-const port = getOptionValue({name: "--port", fallback: "8000", boolean: false, options});
+const port = Number(getOptionValue({name: "--port", fallback: "8000", boolean: false, options}));
 const verbose = getOptionValue({name: "--verbose", fallback: false, boolean: true, options});
 const spa = getOptionValue({name: "--spa", fallback: false, boolean: true, options});
 
-if (Number.isInteger(port)) {
-  console.error("The port must be an integer");
+if (!Number.isInteger(port)) {
+  console.error("The port must be an integer.");
   process.exit(1);
 }
 
